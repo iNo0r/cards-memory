7e99aca55c842fb47aae4ea5939bc16f4e7b8a1a
King <pic.ner101@gmail.com>
2022-06-28 00:04:35 +0100

    initialising Game component

---
+++ src/components/Game.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-c">
+    <section class="section1">
+      <div>Match The boxes</div>
+    </section>
+    <section class="section2"></section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$pn-main-font: "Major Mono Display", monospace;
+
+.main-c {
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 20% 80%;
+}
+
+.section1 {
+  font-family: "Major Mono Display";
+  font-size: xx-large;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
