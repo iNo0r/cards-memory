9efe31548d9bd02e6d1f69fd17932dc98532c156
King <pic.ner101@gmail.com>
2022-07-09 03:04:39 +0100

    third set of svgs

---
+++ src/components/Svgs/vue-icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 45 45"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M32.4 8H39L22.5 37L6 8H18.6225L22.5 14.7246L26.295 8H32.4Z"
+      fill="#41B883"
+    />
+    <path d="M6 8L22.5 37L39 8H32.4L22.5 25.4L12.5175 8H6Z" fill="#41B883" />
+    <path
+      d="M13 8L22.5394 25L32 8H26.166L22.5394 14.5385L18.834 8H13Z"
+      fill="#35495E"
+    />
+  </svg>
+</template>
